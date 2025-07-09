f84c3c58c177e1a0234e3295d0fc3d74fe9cad3a
Christian Müller <christian@flownative.com>
2025-07-09 11:48:44 +0200

    FEATURE: Derive hour format from browser

--- src/components/TopBar/Clock.tsx
+++ src/components/TopBar/Clock.tsx
@@ -3,12 +3,15 @@ import { getTime } from "../../utils/clockUtils";
 import styles from "./clock.module.scss";
 
 const Clock = () => {
-	const [currentTime, setCurrentTime] = useState(getTime(new Date()));
+	const hourCycles = new Intl.Locale(navigator.language).hourCycle ?? ['h12'];
+	const is12HourFormat = ["h11", "h12"].some(hourCycle => hourCycles.includes(hourCycle));
+
+	const [currentTime, setCurrentTime] = useState(getTime(new Date(), is12HourFormat));
 
 	useEffect(() => {
 		let ref = setInterval(() => {
 			let currentTimeObj = new Date();
-			setCurrentTime(getTime(currentTimeObj));
+			setCurrentTime(getTime(currentTimeObj, is12HourFormat));
 		}, 1000);
 		return () => clearInterval(ref);
 	}, []);
@@ -16,7 +19,9 @@ const Clock = () => {
 	return (
 		<div className={styles.clock}>
 			{currentTime.time}
+			{is12HourFormat ? (
 			<span className={styles.meridiem}>{currentTime.meridiem}</span>
+      ) : ''}
 		</div>
 	);
 };
